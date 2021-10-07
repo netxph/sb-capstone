b6f0a02e1f81d948032a59f07e6f3f608bf362fd
Marc Vitalis <marc.vitalis@live.com>
2021-10-07 16:28:27 +0800

    initial baseline

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='sb_capstone',
+    packages=["sb_capstone"],
+    version='0.1.0',
+    description='Starbucks Data Science Capstone Project',
+    author='Marc Vitalis',
+    license='MIT',
+)
